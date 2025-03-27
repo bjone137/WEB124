38a976449fd4c6764ddf306e4bc5ad7e971d4c34
bjone137 <bjone137@stumail.jccc.edu>
2025-03-26 22:20:28 -0500

    Chapter 3 Project upload

---
+++ Chapter3Project/project3.js
@@ -0,0 +1,23 @@
+// Brady Jones 03/30/2025
+let myName = "Brady Owen Jones";
+console.log(myName);
+
+let targetSalary = 65000;
+console.log(targetSalary);
+
+let vetStat = false;
+console.log(vetStat);
+
+let friendNames = ["Jacob", "Nathan", "Yash"]
+console.log(friendNames);
+
+let friendSalary = [60000, 70000, 65000]
+console.log(friendSalary);
+
+let friendInfo = {
+    firstName: "Max",
+    lastName: "Motley",
+    salary: 65000
+};
+console.log(friendInfo);
+
